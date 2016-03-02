79ed019e64214fb9b21a17ce549cdd69a65e3a79
xiaowu20121212 <1078336055@qq.com>
2016-03-02 08:37:19 +0800

    Update MainActivity.java

--- src/com/eebbk/esbrowser/MainActivity.java
+++ src/com/eebbk/esbrowser/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		System.out.println("add new line");
+		System.out.println("add new line");
 	}
 
 	@Override
