22f85a6170d17ae58a7345f06a4543ce75a2c802
dell390 <dell390@zydz102882.dw.gdbbk.com>
2016-03-02 09:45:35 +0800

    delete

--- src/com/eebbk/esbrowser/MainActivity.java
+++ src/com/eebbk/esbrowser/MainActivity.java
@@ -11,19 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		System.out.println("add new line");
-		
+	
 	
 		
 
