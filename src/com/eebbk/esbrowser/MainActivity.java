afd9082cc850a6f62e5bca2c7438e37cc42ae809
Administrator <Administrator@zydz105151.dw.gdbbk.com>
2016-03-02 09:07:45 +0800

    ll

--- src/com/eebbk/esbrowser/MainActivity.java
+++ src/com/eebbk/esbrowser/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 		System.out.println("add new line");
 		System.out.println("add new line");
 		System.out.println("add new line");
+		System.out.println("add new line");
 	}
 
 	@Override
